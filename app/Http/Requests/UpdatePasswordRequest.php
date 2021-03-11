2cfe5ca434714887a3a131b52329afa73548ebe7
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-03-11 15:37:31 -0600

    [MODIFY] Updating the validation rules in UpdatePasswordRequest

--- app/Http/Requests/UpdatePasswordRequest.php
+++ app/Http/Requests/UpdatePasswordRequest.php
@@ -26,7 +26,7 @@ class UpdatePasswordRequest extends FormRequest
         return [
             //
             'password' => 'required',
-            'confirm_password' => 'required'
+            'confirm_password' => 'required|same:password'
         ];
     }
 }

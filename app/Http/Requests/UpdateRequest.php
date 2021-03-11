ab3841cbabc936ff0e3106bcbb0150a7f1dbb385
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-03-11 15:29:09 -0600

    [MODIFY] Adding validators to UpdateRequest

--- app/Http/Requests/UpdateRequest.php
+++ app/Http/Requests/UpdateRequest.php
@@ -13,7 +13,7 @@ class UpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,9 @@ class UpdateRequest extends FormRequest
     {
         return [
             //
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email'
         ];
     }
 }
